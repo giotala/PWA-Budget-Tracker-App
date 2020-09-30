16272f66c3d18ba68b7474cb8894e54e58610db6
Gio Talabong <gio.talabong@gmail.com>
2020-09-29 17:42:23 -0700

    working offline and online

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,6 +1,14 @@
-const FILES_TO_CACHE = ["/", "/index.html", "/index.js", "/styles.css"];
+const FILES_TO_CACHE = [
+    "/",
+    "/index.html",
+    "/index.js",
+    "/styles.css",
+    "/images/mountain.jpg", 
+    "/db.js", 
+    "/manifest.webmanifest"
+];
 
-const CACHE_NAME = "static-cache-v2";
+const CACHE_NAME = "static-cache-v3";
 const DATA_CACHE_NAME = "data-cache-v1";
 
 self.addEventListener("install", function (evt) {
@@ -31,27 +39,36 @@ self.addEventListener("activate", function (evt) {
   self.clients.claim();
 });
 
-self.addEventListener("fetch", function (evt) {
-  if (evt.request.url.includes("./routes/api")) {
-    evt.respondWith(
-      caches
-        .open(DATA_CACHE_NAME)
-        .then((cache) => {
+// fetch
+self.addEventListener("fetch", function(evt) {
+    // cache successful requests to the API
+    if (evt.request.url.includes("/api/")) {
+      evt.respondWith(
+        caches.open(DATA_CACHE_NAME).then(cache => {
           return fetch(evt.request)
-            .then((response) => {
+            .then(response => {
+              // If the response was good, clone it and store it in the cache.
               if (response.status === 200) {
                 cache.put(evt.request.url, response.clone());
               }
-
               return response;
             })
-            .catch((err) => {
+            .catch(err => {
+              // Network request failed, try to get it from the cache.
               return cache.match(evt.request);
             });
-        })
-        .catch((err) => console.log(err))
-    );
+        }).catch(err => console.log(err))
+      );
 
-    return;
-  }
-});
+      return;
+    }
+  
+    // if the request is not for the API, serve static assets using "offline-first" approach.
+    // see https://developers.google.com/web/fundamentals/instant-and-offline/offline-cookbook#cache-falling-back-to-network
+    evt.respondWith(
+      caches.match(evt.request).then(function(response) {
+        return response || fetch(evt.request);
+      })
+    );
+  });
+  
